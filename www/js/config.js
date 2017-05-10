a322ac540f10f24985d7693dd3b9a7fd2e1e84ac
vanderleiot <central@hostbdc.com.br>
2017-05-10 16:11:35 -0300

    JHU

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={	
 	'ApiUrl' : "http://deliver.shopbdc.com.br/mobileapp/api",
-	'DialogDefaultTitle' : "Deliver BDC",
+	'DialogDefaultTitle' : "DELIVERY BDC",
 	'pushNotificationSenderid' : "283086298967",
 	'facebookAppId' : "1905314469715714",
 	'APIHasKey' : "c0c56e6c1e578616ae7527cbbc727a77"
