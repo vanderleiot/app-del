14e180341489866a1fd03a2cda939a7c3d340649
vittrine <central@hostbdc.com.br>
2018-04-21 13:10:53 -0300

    erer

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={					
-    'ApiUrl':"http://deliveryma.com.br/merchantapp/api",	           
-	'DialogDefaultTitle':"Adm Delivery",
+    'ApiUrl':"https://deliveryma.com.br/merchantapp/api",	           
+	'DialogDefaultTitle':"Adm",
 	'pushNotificationSenderid':"283086298967",
 	'APIHasKey':"240d83f3dddde57c457f44522f64e7d9"
 };
