1d12995034002e9a0a7856c99b6aadc124179faa
vanderleiot <central@hostbdc.com.br>
2018-04-21 13:14:59 -0300

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,11 +1,8 @@
 
 var krms_config ={					
     'ApiUrl':"https://deliveryma.com.br/merchantapp/api",	           
-<<<<<<< HEAD
 	'DialogDefaultTitle':"Adm",
-=======
 	'DialogDefaultTitle':"Adm Delivery",
->>>>>>> 7943dd420b6e1139b0634fd8f8c191847abc825c
 	'pushNotificationSenderid':"283086298967",
 	'APIHasKey':"240d83f3dddde57c457f44522f64e7d9"
 };
