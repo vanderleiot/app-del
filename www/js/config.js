a1e1b6afb74d9d235a5a44850979d1b0ff44a07c
vittrine <central@hostbdc.com.br>
2018-04-22 14:45:10 -0300

    bom

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,7 @@
 
 var krms_config ={					
     'ApiUrl':"https://deliveryma.com.br/merchantapp/api",	           
-	'DialogDefaultTitle':"Adm",
-	'DialogDefaultTitle':"Adm Delivery",
+	'DialogDefaultTitle':"Adm DeliveryMA",
 	'pushNotificationSenderid':"283086298967",
 	'APIHasKey':"240d83f3dddde57c457f44522f64e7d9"
 };
