a36324a59412c7987fd100d7589d7b3c919acd57
vanderleiot <central@hostbdc.com.br>
2018-07-20 10:09:13 -0300

    esd

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 
 var krms_config ={					
-    'ApiUrl':"https://deliveryma.com.br/merchantapp/api",	           
-	'DialogDefaultTitle':"Adm DeliveryMA",
+    'ApiUrl':"http://admin.azfidelizi.com/merchantapp/api",	           
+	'DialogDefaultTitle':"AzFidelizi",
 	'pushNotificationSenderid':"283086298967",
 	'APIHasKey':"240d83f3dddde57c457f44522f64e7d9"
 };
